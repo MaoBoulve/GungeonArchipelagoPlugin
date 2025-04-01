4faa8918474a8ab64cd62a2a38a3c74c9219b2b1
Chris M <Christian.mendoza@ucf.edu>
2025-04-01 16:09:26 -0400

    Development environment initial upload

---
+++ ArchipelaGunPlugin.cs
@@ -0,0 +1,110 @@
+﻿using ArchiGungeon.Archipelago;
+using BepInEx;
+
+namespace ArchiGungeon
+{
+    [BepInDependency(Alexandria.Alexandria.GUID)] // this mod depends on the Alexandria API: https://enter-the-gungeon.thunderstore.io/package/Alexandria/Alexandria/
+    [BepInDependency(ETGModMainBehaviour.GUID)]
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class ArchipelaGunPlugin : BaseUnityPlugin
+    {
+        public const string GUID = "maoboulve.etg.archipelagogungeon";
+        public const string NAME = "Archipelago Gungeon Randomizer";
+        public const string VERSION = "0.0.1";
+        public const string TEXT_COLOR = "#B6FFB8";
+
+        public const string MOD_ITEM_PREFIX = "arch";
+
+        public static ArchipelagoGUI ArchipelagoModMenu;
+        public static GungeonPlayerEventListener PlayerListener;
+        public static GameManager GameManagerInstance;
+        private static bool isInit = false;
+
+
+        public void Start()
+        {
+            ETGModMainBehaviour.WaitForGameManagerStart(GMStart);
+            
+        }
+
+        public void GMStart(GameManager g)
+        {
+            GameManagerInstance = g;
+            InitItemHooks();
+            RegisterItems();
+
+            InitModMenu();
+            
+            InitPlayerListener();
+            
+
+            Log($"{NAME} v{VERSION} started successfully.", TEXT_COLOR);
+            isInit = true;
+
+            StartPostInitializationSetup();
+
+            return;
+        }
+
+        private void InitItemHooks()
+        {
+            //CustomBlankModItem.InitHooks();
+        }
+
+        private void RegisterItems()
+        {
+            Archipelagun.Register();
+
+        }
+
+        private void InitModMenu()
+        {
+            ArchipelagoModMenu = new ArchipelagoGUI();
+            ArchipelagoModMenu.Start();
+        }
+
+        
+        private void InitPlayerListener()
+        {
+            PlayerListener = new GungeonPlayerEventListener();
+           
+            return;
+        }
+
+        // Setup functions after instancing all needed functions to prevent missing reference runtime errors
+        private void StartPostInitializationSetup()
+        {
+            StartGungeonPlayerListener();
+
+            // Print all directories BepInEx will allow
+            PlayerPersistentDataHandler.TDD_PrintAllPathsDirectory();
+
+            return;
+        }
+
+        private void StartGungeonPlayerListener()
+        {
+            PlayerListener.StartSystemEventListens();
+            return;
+        }
+
+        public static void Log(string text, string color="#FFFFFF")
+        {
+            ETGModConsole.Log($"<color={color}>{text}</color>");
+        }
+
+        public void Update()
+        {   
+            if ( !isInit )
+            {
+                return;
+            }
+
+            ArchipelagoModMenu.Update();
+            PlayerListener.Update();
+            SessionHandler.Instance?.Update();
+
+            return;
+        }
+    }
+}
---
+++ ItemArchipelago/APItem.cs
@@ -0,0 +1,48 @@
+﻿using Alexandria.ItemAPI;
+using ArchiGungeon.Archipelago;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace ArchiGungeon.ItemArchipelago
+{
+    public class APItem: PassiveItem
+    {
+        public int GungeonAPItemSpawnID = -1;
+        public long ArchipelagoItemID;
+        private static string spriteDirectory = "ArchiGungeon/Resources/archipelago.png";
+        private static string longDesc = "A mysterious item from another world";
+
+        // Dynamically create APItem with names
+        public static APItem RegisterAPItem(long itemIDstring, string passiveName = "APItem", string shortDesc = "Short Description")
+        {
+            GameObject obj = new GameObject(passiveName);
+            var item = obj.AddComponent<APItem>();
+            ItemBuilder.AddSpriteToObject(passiveName, spriteDirectory, obj);
+
+            item.ArchipelagoItemID = itemIDstring;
+
+            ItemBuilder.SetupItem(item, shortDesc, longDesc, ArchipelaGunPlugin.MOD_ITEM_PREFIX);
+
+            item.CanBeDropped = false;
+            item.CanBeSold = false;
+            item.IgnoredByRat = true;
+
+            item.GungeonAPItemSpawnID = PickupObjectDatabase.GetId(item);
+
+            return item;
+        }
+
+        public override void Pickup(PlayerController player)
+        {
+            base.Pickup(player);
+
+            SessionHandler.DataSender.SendFoundLocationCheck(this.ArchipelagoItemID);
+
+            return;
+        }
+
+    }
+}
