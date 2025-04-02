20432cbf4ec60f87154d40b0cf2ec29430eed85b
Chris M <Christian.mendoza@ucf.edu>
2025-04-02 13:21:16 -0400

    Revert "Removing sandbox & reference code"
    This reverts commit 4bc059072f0bc854df9b6e7386a2a90bc475c9a5.

---
+++ ReferenceCode/ItemPassives/PassiveAmmolet.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Alexandria.ItemAPI;
+
+namespace ArchiGungeon
+{
+    class PassiveAmmolet: CustomBlankModItem
+    {
+        private static bool canSpawnBlank;
+
+        public static void Register()
+        {
+            string itemName = "Blankie";
+            string SpriteDirectory = "ArchiGungeon/Resources/example_item_sprite.png";
+
+            GameObject obj = new GameObject(itemName);
+            var item = obj.AddComponent<PassiveAmmolet>();
+            ItemBuilder.AddSpriteToObject(itemName, SpriteDirectory, obj);
+
+            string shortDesc = "Blankie McBlank Face";
+            string longDesc = "What if blanks could blank its own blank?";
+
+
+            ItemBuilder.SetupItem(item, shortDesc, longDesc, "bas");
+
+            ItemBuilder.AddPassiveStatModifier(item, PlayerStats.StatType.AdditionalBlanksPerFloor, 1, StatModifier.ModifyMethod.ADDITIVE);
+
+            item.quality = PickupObject.ItemQuality.S;
+        }
+
+
+        public override void Pickup(PlayerController player)
+        {
+            base.Pickup(player);
+        }
+
+        public override DebrisObject Drop(PlayerController player)
+        {
+            return base.Drop(player);
+        }
+
+        protected override void OnBlank(SilencerInstance silencerInstance, Vector2 centerPoint, PlayerController user)
+        {
+            // base.OnBlank(silencerInstance, centerPoint, user);
+
+            if (canSpawnBlank)
+            {
+                GameObject freeBlank = PickupObjectDatabase.GetById(224).gameObject;
+                LootEngine.SpawnItem(freeBlank, user.CenterPosition, Vector2.zero, 0);
+
+                canSpawnBlank = false;
+            }
+
+            else
+            {
+                canSpawnBlank = true;
+            }
+
+            return;
+        }
+
+    }
+}
